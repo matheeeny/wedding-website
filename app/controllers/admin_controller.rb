408d9031a2ed3a66a3d1f7f0565d6a6db4c793e2
Andrew Matheny <andrew.j.matheny@gmail.com>
2012-06-23 10:55:56 -0400

    reset admin password

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -11,7 +11,7 @@ class AdminController < ApplicationController
   protected
     def authenticate
       authenticate_or_request_with_http_basic do |username, password|
-        username == "lando" && Digest::MD5.hexdigest(password) == "f6a0df19c20f0a82988e559bed9b2cbb"
+        Digest::MD5.hexdigest(password) == "5cdd6c4f1ac4f34210fa9a1202870d4d"
       end
     end
 end
